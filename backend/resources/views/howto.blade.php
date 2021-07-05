8bd00295df204facd981bf46ba0a2aa7b9db98d6
daisuketerashita <ekusiad.tera@icloud.com>
2021-07-05 22:55:56 +0900

    使い方ページの追加

--- backend/resources/views/howto.blade.php
+++ backend/resources/views/howto.blade.php
@@ -7,10 +7,12 @@
 @section('content')
 <div class="howto-container">
     <div class="howto-innner">
-        <h2>①『追加する』ボタンを押す</h2>
+        <h2>①『定型文を作成する』ボタンを押す</h2>
         <img src="{{ asset('images/01_howto.png') }}" alt="">
-        <h2>②『タイトル』『鑑賞日』『サムネイル』を入力する</h2>
+        <h2>②『タイトル』と『定型文』を登録する</h2>
         <img src="{{ asset('images/02_howto.png') }}" alt="">
+        <h2>③『COPY』ボタンを押すと登録した定型文をコピーできる</h2>
+        <img src="{{ asset('images/03_howto.png') }}" alt="">
     </div><!-- howto-innner -->
     <a href="{{ route('index') }}" class='btn btn-info btn-back'>さっそく使ってみる</a>
 </div><!-- howto-container -->
